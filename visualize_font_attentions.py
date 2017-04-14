02aa506527abcc6356161e0e2900448e4ea00920
Yota Toyama <raviqqe@gmail.com>
2017-04-14 17:04:24 +0900

    Add font attention visualizer

---
+++ visualize_font_attentions.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import argparse
+import json
+import os
+import os.path
+
+import gargparse
+import matplotlib.image as image
+import numpy as np
+
+
+gargparse.add_argument('char_file', type=argparse.FileType())
+gargparse.add_argument('font_file', type=argparse.FileType())
+gargparse.add_argument('attention_file', type=argparse.FileType())
+gargparse.add_argument('dest_dir')
+
+
+def main():
+    os.makedirs(gargparse.ARGS.dest_dir, exist_ok=True)
+
+    fonts = np.array(json.load(gargparse.ARGS.font_file), dtype=np.uint8)
+    assert fonts.min() == 0 and fonts.max() == 255
+
+    for char, font, attention in zip(
+            [char.strip() for char in gargparse.ARGS.char_file.readlines()],
+            np.stack([255 - fonts] * 3, axis=-1),
+            np.array(json.load(gargparse.ARGS.attention_file))):
+        new_pixels = np.minimum(
+            font[:, :, 0],
+            255 - (255 * np.sqrt(attention)).astype(np.uint8))
+        font[:, :, 1] = new_pixels
+        font[:, :, 2] = new_pixels
+        image.imsave(os.path.join(gargparse.ARGS.dest_dir,
+                                  hex(ord(char)) + '.img'),
+                     font)
+
+
+if __name__ == '__main__':
+    main()
